510e9622d03967cc8e9e11489428cd03aeb7a6f3
cubimon <cubimon93@gmail.com>
2018-05-13 22:18:12 +0200

    add bash

--- powerlinesystemmonitor/util.py
+++ powerlinesystemmonitor/util.py
@@ -1,6 +1,13 @@
+import subprocess
 
-def noop(pl, content="", highlight_group="noop"):
+def noop(pl, content="", highlight_groups=["noop"]):
     return [{
         "contents": content,
-        "highlight_groups": [highlight_group]
+        "highlight_groups": highlight_groups
+            }]
+
+def bash(pl, command="", highlight_groups=["bash"]):
+    return [{
+        "contents": subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).stdout.read().strip().decode("utf-8"),
+        "highlight_groups": highlight_groups
             }]
